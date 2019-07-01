ef07ec349f8f12c91b9ac71c7f9e912812e38ea1
lyine <liuzhengyuan@hotmail.com>
2019-07-01 15:43:09 +0800

    first commit

---
+++ iris.py
@@ -0,0 +1,28 @@
+from sklearn import datasets
+from sklearn import svm
+from sklearn.semi_supervised import label_propagation
+
+import numpy as np
+
+class IrisClassifier:
+    def __init__(self):
+        self.train_simple()
+
+    def get_train_data(self):
+        data,target = datasets.load_iris(return_X_y=True)
+        X = data
+        y = target
+        return X,y
+
+    def train_simple(self):
+        X,y=self.get_train_data()
+        svc=svm.SVC(gamma='auto')
+        self.svc=svc.fit(X,y)
+
+    def predict(self,data):
+        result=self.svc.predict(data)
+        return result
+
+if __name__ == "__main__":
+    classifier=IrisClassifier()
+    print(classifier.predict([[5.9 ,3.,  5.1 ,1.8]]))
